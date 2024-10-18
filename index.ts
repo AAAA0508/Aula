1a0f9ea24565e5dfca0247c5e16ae9ab96ab88a5
unknown <AMABILLY>
2024-10-17 21:52:14 -0300

    meu primeiro commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Olá");
+
+//git int -> inicializa o git.
+
+//git remote add origin -> cria um link entre o repositório local 
+//e o repositório no provedor de repositórios.
+
+//git add . adiciona os arquivos no repositório.
+
+//git status -> lista os arquivos (adicionados , modificados)
